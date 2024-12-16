4157fc53e4a81176aff6e530b87711725079a764
demise twilight <lizijie7471619@outlook.com>
2024-12-16 14:02:29 +0800

    生成的示例脚本

---
+++ Assets/LitMVC/MVC/Logic/TestPageLogic.cs
@@ -0,0 +1,4 @@
+namespace LitMVC {
+	public partial class TestPageView : UIView {
+	}
+}
---
+++ Assets/LitMVC/MVC/View/TestPageView.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using LitMVC;
+namespace LitMVC {
+	public partial class TestPageView : UIView {
+		public Image m_img_bg;
+	}
+}
